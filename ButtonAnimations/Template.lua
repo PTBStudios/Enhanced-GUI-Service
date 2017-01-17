cc0d91e13d656da8d76fe113ec907b555414806c
h5sROBLOX <virtualdy.lan5@gmail.com>
2017-01-17 16:51:14 -0500

    Create Template.lua

---
+++ ButtonAnimations/Template.lua
@@ -0,0 +1,10 @@
+function MouseEnter(Button)	end
+function MouseLeave(Button)	end
+function MouseButton1Down(Button) end
+function MouseButton1Up(Button) end
+function MouseButton1Click(Button) end
+function MouseButton2Down(Button) end
+function MouseButton2Up(Button) end
+function MouseButton2Click(Button) end
+
+return {MouseEnter, MouseLeave, MouseButton1Down, MouseButton1Up, MouseButton1Click, MouseButton2Click, MouseButton2Down, MouseButton2Up}
